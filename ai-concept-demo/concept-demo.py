76cdae1ac163194bb4113bb53ecbadabd4642dbe
Ki Yung Ahn <kyagrd@gmail.com>
2024-05-01 21:57:44 +0900

    Linux font problem ad-hoc fix

--- ai-concept-demo/concept-demo.py
+++ ai-concept-demo/concept-demo.py
@@ -9,10 +9,6 @@ from matplotlib import font_manager, rc
 script_path = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_path)
 
-font_dir = os.path.join(script_dir, './data')
-font_path = os.path.join(font_dir, 'malgun.ttf')
-font_name = font_manager.FontProperties(fname=font_path).get_name()
-
 # 시스템 확인 후 폰트 지정 함수
 def get_system_font():
     system = platform.system()
@@ -23,8 +19,9 @@ def get_system_font():
         # macOS의 경우, 기본 시스템 폰트 경로 설정 (MacOS 애플고딕의 정확한 이름 표기가 필요함)
         return ['/System/Library/Fonts/AppleGothic.ttf']  
     else:
-        # 기타 운영 체제의 경우, 적절한 시스템 폰트 경로를 설정해야 합니다. (현재 리눅스의 파일 경로에 맞춰 설정되어있음)
-        return ['/usr/share/fonts/truetype/NanumGothic.ttf'] 
+        # 기타 운영 체제의 경우, 적절한 시스템 폰트 경로를 설정해야 합니다. (현재 리눅스의 파일 경로에 맞춰 설정되어있음) ===> 리눅스에 맞지 않음!! 배포판마다 위치 다름 이런 하드코딩 정말 안좋습니다
+        # return ['/usr/share/fonts/truetype/NanumGothic.ttf'] 
+        return None
 
 def read_subjects(filename):
     try:
@@ -63,10 +60,14 @@ def draw_course_structure(subjects):
     system_fonts = get_system_font()
     if system_fonts:
         font_path = system_fonts[0]
+        font_name = font_manager.FontProperties(fname=font_path).get_name()
     else:
-        print("시스템 폰트를 찾을 수 없습니다.", file=sys.stderr)
-        sys.exit(1)
-    font_name = font_manager.FontProperties(fname=font_path).get_name()
+        # 무조건 NanumGothic 시도하도록 임시로
+        # path를 하드코딩 해놔서 리눅스에서 돌아가지 않음 배포판마다 다른데 ... 빨리 개선 필요
+        # # print("시스템 폰트를 찾을 수 없습니다.", file=sys.stderr)
+        # # sys.exit(1)
+        font_name = "NanumGothic"
+        
     G = nx.DiGraph()
     adjusted_pos = adjust_coordinates(subjects)
     plt.figure(figsize=(10,10)) # figure 사이즈 조정
