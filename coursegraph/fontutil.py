de43ebf677a002984451e4deb2d296e4430fa050
김준섭 <162093522+kimjunseop716@users.noreply.github.com>
2024-05-02 22:39:24 +0900

    Update fontutil.py

--- coursegraph/fontutil.py
+++ coursegraph/fontutil.py
@@ -1,7 +1,5 @@
 # 한글 폰트 정보를 가져오는 모듈
 # 제발 ttf 파일 path를 하드코딩하지 말 것!!!
-<<<<<<< HEAD
-=======
 import os
 import platform
 from matplotlib import font_manager
@@ -29,4 +27,3 @@ def get_system_font(): # 시스템 확인 후 폰트 지정 함수
         # 기타 운영 체제의 경우, 적절한 시스템 폰트 경로를 설정해야 합니다. (현재 리눅스의 파일 경로에 맞춰 설정되어있음) ===> 리눅스에 맞지 않음!! 배포판마다 위치 다름 이런 하드코딩 정말 안좋습니다
         # return ['/usr/share/fonts/truetype/NanumGothic.ttf']
         return None
->>>>>>> fontpath_and_img
