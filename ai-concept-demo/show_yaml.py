9f8ab188c30f2dd4ad29dfb54718b5ae45719279
lunatic280 <https://github.com/lunatic280>
2024-05-02 13:29:08 +0900

    다른 yaml 파일도 볼 수 있게 수정

--- ai-concept-demo/show_yaml.py
+++ ai-concept-demo/show_yaml.py
@@ -6,23 +6,31 @@ import platform
 import matplotlib.pyplot as plt
 from matplotlib import font_manager, rc
 
+
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
 # 파일 경로 설정
 #파일의 이름을 받아서 파일의 정보 출력하기
 #inpuy.yaml은 출력이 가능하나 다른 파일들은 경로가 달라 경로 지정하고 filename입력
-input_filename = input("yaml파일을 입력하세요(ex -> me.yaml은 me만 입력)")
-script_path = os.path.abspath(__file__)
-script_dir = os.path.dirname(script_path)
-#input.yaml은 그대로 입력
-#다른 파일들은 경로가 다르기 떄문에 다른 주소로 이동
-if input_filename == 'input':
-        filename = os.path.join(script_dir, 'input.yaml')
-else:
-    filename = os.path.join(script_dir, '../data/' + input_filename + '.yaml')
+def input_filename():
 
-font_dir = os.path.join(script_dir, './data')
-font_path = os.path.join(font_dir, 'malgun.ttf')
-font_name = font_manager.FontProperties(fname=font_path).get_name()
+    input_filename = input("yaml파일을 입력하세요(ex -> me.yaml은 me만 입력)")
+    script_path = os.path.abspath(__file__)
+    script_dir = os.path.dirname(script_path)
+    #input.yaml은 그대로 입력
+    #다른 파일들은 경로가 다르기 떄문에 다른 주소로 이동
+    if input_filename == 'input':
+            filename = os.path.join(script_dir, 'input.yaml')
+            
+    else:
+        filename = os.path.join(script_dir, '../data/' + input_filename + '.yaml')
 
+    font_dir = os.path.join(script_dir, './data')
+    font_path = os.path.join(font_dir, 'malgun.ttf')
+    font_name = font_manager.FontProperties(fname=font_path).get_name()
+    # 폰트 설정
+    plt.rcParams['font.family'] = font_name
+    return filename
 
 def get_system_font():
     system = platform.system()
@@ -47,43 +55,38 @@ def read_subjects(filename):
         print("파일을 찾을 수 없습니다.")
         return None
         exit(1)
-# 폰트 설정
-plt.rcParams['font.family'] = font_name
+
 # 파일 읽기
 
-try:
-    with open(filename, 'r', encoding='UTF8') as file:
-        yaml_data = file.read()
-        data = syaml.load(yaml_data).data
-except FileNotFoundError:
-    print("파일을 찾을 수 없습니다.")
-    exit(1)
 
-# 데이터 가공
-df = pd.DataFrame(data['과목'])
-try:
-    df['과목명'] = df['과목명']
-    df['학년'] = df['학년']
-    df['학기'] = df['학기']
-    df['트랙'] = df['트랙']
-    df['마이크로디그리'] = df['마이크로디그리']
-    df['실습여부'] = df['실습여부']
-    df['선수과목'] = df['선수과목'].apply(lambda x: ', '.join(x) if isinstance(x, list) else '')  # 선수과목이 리스트인 경우에만 join 적용
-except KeyError:    
-    
-    pass
+def make_data(data):
+    # 데이터 가공
+    df = pd.DataFrame(data['과목'])
+    try:
+        df['과목명'] = df['과목명']
+        df['학년'] = df['학년']
+        df['학기'] = df['학기']
+        df['트랙'] = df['트랙']
+        df['마이크로디그리'] = df['마이크로디그리']
+        df['실습여부'] = df['실습여부']
+        df['선수과목'] = df['선수과목'].apply(lambda x: ', '.join(x) if isinstance(x, list) else '')  # 선수과목이 리스트인 경우에만 join 적용
+    except KeyError:    
+        
+        pass
 
 
-fig, ax = plt.subplots(figsize=(20, 10))
-ax.axis('tight')
-ax.axis('off')
-ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center', colWidths=[0.2]*len(df.columns))
-ax.set_title('과목 표')
+    fig, ax = plt.subplots(figsize=(20, 10))
+    ax.axis('tight')
+    ax.axis('off')
+    ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center', colWidths=[0.2]*len(df.columns))
+    ax.set_title('과목 표')
 
-plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
-    filename = os.path.join(script_dir, './input.yaml')
+    filename = input_filename()
     subjects = read_subjects(filename)
-    
+    get_system_font()
+    data = read_subjects(filename)
+    make_data(data)
