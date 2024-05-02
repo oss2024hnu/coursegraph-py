b96b9184d861ba4dfe7c68cd9148449708fc98fd
Roviil <qowjdgns0106@naver.com>
2024-05-02 13:25:51 +0900

    MacOS글자 깨짐 해결

--- ai-concept-demo/concept-demo.py
+++ ai-concept-demo/concept-demo.py
@@ -9,15 +9,33 @@ from matplotlib import font_manager, rc
 script_path = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_path)
 
-# 시스템 확인 후 폰트 지정 함수
+# OS 확인 후 폰트 선택 하도록 수정
+#맥OS에서 글자 깨짐 현상 해결
 def get_system_font():
+    flist = font_manager.findSystemFonts()
+    data_list = []
     system = platform.system()
+
     if system == 'Windows':
         # Windows의 경우, 시스템 폰트 경로로 설정
-        return ['C:/Windows/Fonts/malgun.ttf']
+        for v in flist:
+            try:
+                fprop = font_manager.FontProperties(fname=v)
+                if fprop.get_name() == 'Malgun':
+                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
+            except:
+                continue
+        return [data_list[0]['file']]
     elif system == 'Darwin':
+        for v in flist:
+            try:
+                fprop = font_manager.FontProperties(fname=v)
+                if fprop.get_name() == 'Apple SD Gothic Neo':
+                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
+            except:
+                continue
         # macOS의 경우, 기본 시스템 폰트 경로 설정 (MacOS 애플고딕의 정확한 이름 표기가 필요함)
-        return ['/System/Library/Fonts/AppleGothic.ttf']  
+        return [data_list[0]['file']]
     else:
         # 기타 운영 체제의 경우, 적절한 시스템 폰트 경로를 설정해야 합니다. (현재 리눅스의 파일 경로에 맞춰 설정되어있음) ===> 리눅스에 맞지 않음!! 배포판마다 위치 다름 이런 하드코딩 정말 안좋습니다
         # return ['/usr/share/fonts/truetype/NanumGothic.ttf'] 
