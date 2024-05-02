d0e29c496bc74418dccc5ca5867b1990c65fbc71
jimin <jimin@jiminui-MacBookPro.local>
2024-05-02 13:32:41 +0900

    macOS font name

--- ai-concept-demo/concept-demo.py
+++ ai-concept-demo/concept-demo.py
@@ -17,7 +17,7 @@ def get_system_font():
         return ['C:/Windows/Fonts/malgun.ttf']
     elif system == 'Darwin':
         # macOS의 경우, 기본 시스템 폰트 경로 설정 (MacOS 애플고딕의 정확한 이름 표기가 필요함)
-        return ['/System/Library/Fonts/AppleGothic.ttf']  
+        return ['/System/Library/Fonts/AppleSDGothicNeo.ttc']  
     else:
         # 기타 운영 체제의 경우, 적절한 시스템 폰트 경로를 설정해야 합니다. (현재 리눅스의 파일 경로에 맞춰 설정되어있음) ===> 리눅스에 맞지 않음!! 배포판마다 위치 다름 이런 하드코딩 정말 안좋습니다
         # return ['/usr/share/fonts/truetype/NanumGothic.ttf'] 
