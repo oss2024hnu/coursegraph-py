78a2535c13e2422cacab7227420455a41f8d6049
Bae <qowjdgns0106@naver.com>
2024-05-04 15:05:08 +0900

    #99 - 학/강/실 표시 폰트 파일 모듈화

--- coursegraph/fontutil.py
+++ coursegraph/fontutil.py
@@ -1,40 +1,46 @@
-# 한글 폰트 정보를 가져오는 모듈
-# 제발 ttf 파일 path를 하드코딩하지 말 것!!!
 
+# 폰트 관련 파일 모듈화
+import pandas as pd
+import strictyaml as syaml
 import os
-import sys
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
 import platform
-from matplotlib import font_manager
+
 
 def get_system_font():
+    flist = font_manager.findSystemFonts()
+    data_list = []
     system = platform.system()
+
     if system == 'Windows':
-        # Windows의 경우, malgun 폰트 사용
-        font_paths = font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
-        malgun_fonts = [font for font in font_paths if 'malgun.ttf' in font.lower()]
-        if malgun_fonts:
-            return malgun_fonts
-        else:
-            print("Malgun 폰트를 찾을 수 없습니다.", file=sys.stderr)
+        # Windows의 경우, 시스템 폰트 경로로 설정
+        for v in flist:
+            try:
+                fprop = font_manager.FontProperties(fname=v)
+                if fprop.get_name() == 'Malgun':
+                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
+            except:
+                continue
+        return data_list
+    
     elif system == 'Darwin':
-        # macOS의 경우, AppleGothic 폰트 사용(MacOS의 경우 ttf 가 아닌 otf를 사용하는 경우가 존재하기에 ttf파일이 없을시 otf파일을 검색)
-        font_paths = font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
-        apple_gothic_fonts_ttf = [font for font in font_paths if 'AppleGothic.ttf' in font.lower()]
-        if apple_gothic_fonts_ttf:
-            return apple_gothic_fonts_ttf
-        else:
-            # ttf 폰트가 없을 경우 otf 폰트로 다시 시도
-            font_paths_otf = font_manager.findSystemFonts(fontpaths=None, fontext='otf')
-            apple_gothic_fonts_otf = [font for font in font_paths_otf if 'AppleGothic.otf' in font.lower()]
-            if apple_gothic_fonts_otf:
-                return apple_gothic_fonts_otf
-            else:
-                print("AppleGothic 폰트를 찾을 수 없습니다.", file=sys.stderr)
+        for v in flist:
+            try:
+                fprop = font_manager.FontProperties(fname=v)
+                if fprop.get_name() == 'Apple SD Gothic Neo':
+                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
+            except:
+                continue
+        return data_list
+    
     else:
-        # 리눅스 및 기타 OS의 경우, NanumGothic 폰트 사용
-        font_paths = font_manager.findSystemFonts(fontpaths=None, fontext='ttf')
-        nanum_gothic_fonts = [font for font in font_paths if 'NanumGothic.ttf' in font.lower()]
-        if nanum_gothic_fonts:
-            return nanum_gothic_fonts
-        else:
-            print("NanumGothic 폰트를 찾을 수 없습니다.", file=sys.stderr)
+        for v in flist:
+            try:
+                fprop = font_manager.FontProperties(fname=v)
+                if fprop.get_name() == 'NanumGothic':
+                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
+            except:
+                continue
+        return data_list
+
