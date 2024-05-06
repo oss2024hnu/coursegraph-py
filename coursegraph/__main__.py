ffd0b16672ef6d4c3746c85e3a22a7246bd87954
Han <lovingamtist@gmail.com>
2024-05-07 01:59:44 +0900

    file update main modified

--- coursegraph/__main__.py
+++ coursegraph/__main__.py
@@ -5,6 +5,7 @@ import sys
 from show_yaml import ShowYaml
 import fontutil
 from tkinter import filedialog
+from parsing import parse_arguments
 
 def open_select_yaml():
     root = tk.Tk()
@@ -14,17 +15,7 @@ def open_select_yaml():
     return select_yaml
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='A CLI utility for processing data.',
-        epilog='Enjoy using the CLI utility!'
-    )
-
-    # Adding command line options
-    parser.add_argument('-i', '--input', type=str, help='Specify the input file path.')
-    parser.add_argument('-o', '--output', type=str, help='Specify the output file path.')
-    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode.')
-    parser.add_argument('-s', '--show', action='store_true', help='Show data.')
-    args = parser.parse_args()
+    args = parse_arguments()
 
     # Accessing the command line options
     input_file = args.input
