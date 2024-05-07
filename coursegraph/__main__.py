49a35cf507cf3c22513071721a7aa4fa41679f79
tkdgns08523 <tkdgns08523@naver.com>
2024-05-07 22:00:35 +0900

    q wq

--- coursegraph/__main__.py
+++ coursegraph/__main__.py
@@ -7,13 +7,22 @@ import fontutil
 from tkinter import filedialog
 
 def open_select_yaml():
+  try:
+
     root = tk.Tk()
     root.withdraw()
 
     select_yaml = filedialog.askopenfilename(initialdir="../data", title="Select file", filetypes=(("YAML files", "*.yaml"), ("all files", "*.*")))
     return select_yaml
+  
+  # 오류가 발생할 때 비정상적으로 종료되지 않도록 합니다
+  except tk.TclError as e:
+        print(f"An error occurred while opening file dialog: {e}")
+        sys.exit(1)
 
 def main():
+  try:
+
     parser = argparse.ArgumentParser(
         description='A CLI utility for processing data.',
         epilog='Enjoy using the CLI utility!'
@@ -45,7 +54,11 @@ def main():
     if show_data:
         data_processor = ShowYaml()
         data_processor.process_data()
-      
+    
+  except Exception as e:
+      print(f"An error occurred: {e}")
+      sys.exit(1)
+
     # Add more functionality based on your application needs
 
 if __name__ == '__main__':
