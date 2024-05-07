9e30b93ae4621b2f3936f4491a3a7a2f548a3e8a
SiRyung <joon@naver.com>
2024-05-07 17:24:39 +0900

    forked repo updates

--- coursegraph/__main__.py
+++ coursegraph/__main__.py
@@ -1,5 +1,17 @@
 import argparse
+import tkinter as tk
+from tkinter import filedialog
+import sys
+from show_yaml import ShowYaml
 import fontutil
+from tkinter import filedialog
+
+def open_select_yaml():
+    root = tk.Tk()
+    root.withdraw()
+
+    select_yaml = filedialog.askopenfilename(initialdir="../data", title="Select file", filetypes=(("YAML files", "*.yaml"), ("all files", "*.*")))
+    return select_yaml
 
 def main():
     parser = argparse.ArgumentParser(
@@ -11,13 +23,14 @@ def main():
     parser.add_argument('-i', '--input', type=str, help='Specify the input file path.')
     parser.add_argument('-o', '--output', type=str, help='Specify the output file path.')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode.')
-
+    parser.add_argument('-s', '--show', action='store_true', help='Show data.')
     args = parser.parse_args()
 
     # Accessing the command line options
     input_file = args.input
     output_file = args.output
     verbose_mode = args.verbose
+    show_data = args.show
 
     # Perform actions based on options
     if verbose_mode:
@@ -27,10 +40,13 @@ def main():
         print(f"Output file path: {output_file}")
 
     if input_file:
-        print(f"Output file path: {output_file}")
-
+        print(f"Output file path: {input_file}")
+    
+    if show_data:
+        data_processor = ShowYaml()
+        data_processor.process_data()
+      
     # Add more functionality based on your application needs
-
 
 if __name__ == '__main__':
     main()
--- coursegraph/fontutil.py
+++ coursegraph/fontutil.py
@@ -1,2 +1,46 @@
-# 한글 폰트 정보를 가져오는 모듈
-# 제발 ttf 파일 path를 하드코딩하지 말 것!!!
+
+# 폰트 관련 파일 모듈화
+import pandas as pd
+import strictyaml as syaml
+import os
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+import platform
+
+
+def get_system_font():
+    flist = font_manager.findSystemFonts()
+    data_list = []
+    system = platform.system()
+
+    if system == 'Windows':
+        # Windows의 경우, 시스템 폰트 경로로 설정
+        for v in flist:
+            try:
+                fprop = font_manager.FontProperties(fname=v)
+                if fprop.get_name() == 'Malgun':
+                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
+            except:
+                continue
+        return data_list
+    
+    elif system == 'Darwin':
+        for v in flist:
+            try:
+                fprop = font_manager.FontProperties(fname=v)
+                if fprop.get_name() == 'Apple SD Gothic Neo':
+                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
+            except:
+                continue
+        return data_list
+    
+    else:
+        for v in flist:
+            try:
+                fprop = font_manager.FontProperties(fname=v)
+                if fprop.get_name() == 'NanumGothic':
+                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
+            except:
+                continue
+        return data_list
+
