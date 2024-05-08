37a641ed1d0e697bdc00d156ffa11011cdbe345f
Ki Yung Ahn <kyagrd@gmail.com>
2024-05-09 02:00:40 +0900

    separate gui to another module

--- coursegraph/__main__.py
+++ coursegraph/__main__.py
@@ -1,25 +1,8 @@
 import argparse
-import tkinter as tk
-from tkinter import filedialog
 import sys
-from show_yaml import ShowYaml
+
 import fontutil
-
-
-
-def open_select_yaml():
-  try:
-
-    root = tk.Tk()
-    root.withdraw()
-
-    select_yaml = filedialog.askopenfilename(initialdir="../data", title="Select file", filetypes=(("YAML files", "*.yaml"), ("all files", "*.*")))
-    return select_yaml
-  
-  # 오류가 발생할 때 비정상적으로 종료되지 않도록 합니다
-  except tk.TclError as e:
-        print(f"An error occurred while opening file dialog: {e}")
-        sys.exit(1)
+from show_yaml import ShowYaml
 
 def main():
   try:
