246ca91d2cd6c678be3eb627c8ed7377f20dc9d5
jangmuk yoo(유장묵) <ghve3691@naver.com>
2024-05-08 20:49:54 +0900

    requirements.txt 위치를 다시 최상위 디렉터리에 위치 requirements.txt 파일 경로를 가져오도록 모듈을 다시 고침

--- coursegraph/package_installer.py
+++ coursegraph/package_installer.py
@@ -1,14 +1,34 @@
 import subprocess
 import sys
+import os
 
 
 class PackageInstaller:
     def __init__(self, requirements_file="requirements.txt"):
         self.requirements_file = requirements_file
 
+    def find_top_dir(self):
+        current_dir = os.path.abspath(os.path.dirname(__file__))
+
+        while not os.path.exists(os.path.join(current_dir, self.requirements_file)):
+            parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
+
+            if parent_dir == current_dir:
+                return None
+
+            current_dir = parent_dir
+        return current_dir
+
     def install_packages(self):
+        top_level_dir = self.find_top_dir()
+        if not top_level_dir:
+            print("Error : requirements.txt file not found ")
+            return
+
+        requirements_path = os.path.join(top_level_dir, self.requirements_file)
+
         try:
-            with open(self.requirements_file, "r") as f:
+            with open(requirements_path, "r") as f:
                 required_packages = [line.strip() for line in f.readlines() if line.strip()]
         except FileNotFoundError:
             print(f"Error: {self.requirements_file} not found.")
@@ -16,7 +36,7 @@ class PackageInstaller:
 
         if required_packages:
             try:
-                subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", self.requirements_file])
+                subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", requirements_path])
                 print("packages installed successfully.")
             except subprocess.CalledProcessError as e:
                 print(f"Error occurred while installing packages: {e}")
