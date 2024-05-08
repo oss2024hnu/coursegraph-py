43dcbef8da92924a84e647105ba6a4f7e4c80413
eong <eongeung1022@gmail.com>
2024-05-08 18:48:45 +0900

    이미지 모드 활성화 및 yaml 파일 입력, 실행 후 시각화까지 완료했습니다.

--- coursegraph/fontutil.py
+++ coursegraph/fontutil.py
@@ -9,38 +9,15 @@ import platform
 
 
 def get_system_font():
-    flist = font_manager.findSystemFonts()
-    data_list = []
     system = platform.system()
-
     if system == 'Windows':
         # Windows의 경우, 시스템 폰트 경로로 설정
-        for v in flist:
-            try:
-                fprop = font_manager.FontProperties(fname=v)
-                if fprop.get_name() == 'Malgun':
-                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
-            except:
-                continue
-        return data_list
-    
+        return ['C:/Windows/Fonts/malgun.ttf']
     elif system == 'Darwin':
-        for v in flist:
-            try:
-                fprop = font_manager.FontProperties(fname=v)
-                if fprop.get_name() == 'Apple SD Gothic Neo':
-                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
-            except:
-                continue
-        return data_list
-    
+        # macOS의 경우, 기본 시스템 폰트 경로 설정 (MacOS 애플고딕의 정확한 이름 표기가 필요함)
+        return ['/System/Library/Fonts/AppleSDGothicNeo.ttc']
     else:
-        for v in flist:
-            try:
-                fprop = font_manager.FontProperties(fname=v)
-                if fprop.get_name() == 'NanumGothic':
-                    data_list.append({"name": fprop.get_name(), "file": fprop.get_file()})
-            except:
-                continue
-        return data_list
+        # 기타 운영 체제의 경우, 적절한 시스템 폰트 경로를 설정해야 합니다. (현재 리눅스의 파일 경로에 맞춰 설정되어있음) ===> 리눅스에 맞지 않음!! 배포판마다 위치 다름 이런 하드코딩 정말 안좋습니다
+        # return ['/usr/share/fonts/truetype/NanumGothic.ttf']
+        return None
 
--- coursegraph/show_yaml.py
+++ coursegraph/show_yaml.py
@@ -4,46 +4,60 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib import font_manager, rc
 
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from coursegraph import fontutil as font #폰트 모듈 호출
-import platform
+# fontutil 모듈이 아닌 직접 시스템 폰트 경로 지정
+def get_system_font():
+    # 예시로 Windows 폰트 경로 지정
+    return 'C:/Windows/Fonts/malgun.ttf'
 
-# 파일 경로 설정
-script_path = os.path.abspath(__file__)
-script_dir = os.path.dirname(script_path)
-filename = os.path.join(script_dir, './input.yaml')
+class ShowYaml:
+    def __init__(self, image_mode=False):  # 기본값으로 False 설정
+        self.script_dir = os.path.dirname(os.path.abspath(__file__))
+        self.font_path = get_system_font()
+        self.filename = self.input_filename()
+        self.image_mode = image_mode
 
-font_dir = os.path.join(script_dir, './data')
-font_path = os.path.join(font_dir, 'malgun.ttf')
-font_name = font_manager.FontProperties(fname=font_path).get_name()
+    def input_filename(self):
+        input_filename = input("yaml 파일을 입력하세요 (예: me.yaml은 me 입력): ")
+        # 데이터 디렉토리가 한 단계 위에 위치하는 상황을 가정
+        filename = os.path.join(self.script_dir, '..', 'data', input_filename + '.yaml')
+        return filename
 
-# 폰트 설정
-fonts = font.get_system_font()
-plt.rcParams['font.family'] = fonts[0]['name']
+    def read_subjects(self):
+        try:
+            with open(self.filename, 'r', encoding='UTF8') as file:
+                yaml_data = file.read()
+                data = syaml.load(yaml_data).data
+                return data
+        except FileNotFoundError:
+            print("파일을 찾을 수 없습니다.")
+            return None
+        except Exception as e:
+            print("파일을 읽는 중 오류가 발생했습니다:", e)
+            return None
 
+    def make_data(self, data):
+        font_name = font_manager.FontProperties(fname=self.font_path).get_name()
+        rc('font', family=font_name)
 
+        if '과목' in data:
+            df = pd.DataFrame(data['과목'])
+            fig, ax = plt.subplots(figsize=(20, 10))
+            ax.axis('off')
+            ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center', colWidths=[0.2]*len(df.columns))
+            ax.set_title('과목 표')
+            if self.image_mode:
+                filename_without_ext = os.path.splitext(os.path.basename(self.filename))[0]
+                plt.savefig(os.path.join(self.script_dir, filename_without_ext + '_chart_image.png'))
+            plt.show()
+        else:
+            print("데이터에 '과목' 정보가 없습니다.")
 
-# 파일 읽기
-try:
-    with open(filename, 'r', encoding='UTF8') as file:
-        yaml_data = file.read()
-        data = syaml.load(yaml_data).data
-except FileNotFoundError:
-    print("파일을 찾을 수 없습니다.")
-    exit(1)
+    def process_data(self):
+        subjects = self.read_subjects()
+        if subjects:
+            self.make_data(subjects)
 
-# 데이터 가공
-df = pd.DataFrame(data['과목'])
-df['과목명'] = df['과목명']
-df['학년'] = df['학년']
-df['학기'] = df['학기']
-df['학/강/실'] = df['학/강/실']
-df['선수과목'] = df['선수과목'].apply(lambda x: ', '.join(x) if isinstance(x, list) else '')  # 선수과목이 리스트인 경우에만 join 적용
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.axis('tight')
-ax.axis('off')
-ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center', colWidths=[0.2]*len(df.columns))
-ax.set_title('과목 표')
-
-plt.show()
+if __name__ == "__main__":
+    image_mode = input("이미지 모드를 활성화하시겠습니까? (yes/no): ").lower() == 'yes'
+    data_processor = ShowYaml(image_mode)
+    data_processor.process_data()
