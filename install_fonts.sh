13c0f08485df3be3ec4339f199133dbf6a875697
Ki Yung Ahn <kyagrd@gmail.com>
2024-05-09 14:47:37 +0900

    Add comment inside install_fonts.sh

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,3 +1,5 @@
+# Hangul font installation script for Debian/Ubuntu linux distro.
+# Use this script to view Hangul in Github Codespace.
 sudo apt-get update -y
 sudo apt-get install -y fonts-nanum
 sudo fc-cache -fv
