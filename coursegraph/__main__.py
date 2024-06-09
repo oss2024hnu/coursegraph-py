2fa5697efd11678159c13633b90f9995890d547f
박재원(jaewon) <127334443+chuunbyul@users.noreply.github.com>
2024-06-09 11:43:25 +0900

    Update __main__.py

--- coursegraph/__main__.py
+++ coursegraph/__main__.py
@@ -86,9 +86,9 @@ def main():
             ref = draw_course_structure(subjects, output_file, width, height)
             if args.verbose == 2:
                 cliprint(ref)
-        elif output_format == 'table':
             # kyahnu: 이 부분 --input 과 --output 을 활용하도록 일관된 인터페이스로 수정할 것
-            data_processor = ShowTable(not show_mode, input_file, output_file,width,height)
+        elif output_format in ['table', 'pdf']:
+            data_processor = ShowTable(not show_mode, input_file, output_file, width, height)
             data_processor.process_data()
         else:
             raise Exception(f"cannot handle output format {output_format}")
