8f9efaa48514b5370acccdf79c3614904a9b2b1c
tkdgns08523 <tkdgns08523@naver.com>
2024-06-14 17:34:03 +0900

    graph 다시 수정

--- coursegraph/show_graph.py
+++ coursegraph/show_graph.py
@@ -18,6 +18,16 @@ class EdgeAttributes:
 
 
 def read_subjects(filename: str) -> Optional[strictyaml.YAML]:
+    """
+    파일의 경로로 파일 열기를 시도합니다. 그리고 데이터 파일의 유효성을 검사하여, 에러를 처리하는 함수입니다.
+
+    Parameter:
+    filename (str) : 파일의 경로
+
+    return:
+    유효한 경우 '과목' 의 키값들을 strictyaml.YAML 유형으로 리턴합니다. 유효하지 않은 경우 None 을 반환합니다.
+    """
+
     try:
         with open(filename, 'r', encoding='UTF8') as file:
             yaml_data = file.read()
@@ -32,6 +42,16 @@ def read_subjects(filename: str) -> Optional[strictyaml.YAML]:
 
 
 def adjust_coordinates(subjects: Optional[strictyaml.YAML]) -> Dict[int, List[float]]:
+    """
+    학년과 학기가 같은 강좌에 대한 좌표 조정 함수입니다.
+
+    Parameter:
+    subjects (Optional[strictyaml.YAML]): strictyaml.YAML 유형의 데이터를 받습니다.
+
+    return:
+    좌표가 조정되어야 할 부분이 dict 자료형으로 반환됩니다.
+    """
+
     if subjects is None:
         return {}
 
@@ -58,12 +78,34 @@ def adjust_coordinates(subjects: Optional[strictyaml.YAML]) -> Dict[int, List[fl
     return adjusted_pos
 
 def cliprint(ref: dict):
+
+    """
+    CLI 상에서 ref 의 값을 출력해주는 함수입니다.
+    
+    Parameter:
+    ref (dict) : 키값이 과목, 학기 들로 구성되어 있는 딕셔너리입니다.
+
+    return:
+    해당 함수는 반환값이 없습니다.
+    """
+    
     sorted_ref = dict(sorted(ref.items()))
     for key, value in sorted_ref.items():
         print(f"{key}: {value}")
 
 
 def get_edge_color(category: str) -> str:
+
+    """
+    각 edge 의 컬러를 결정해주는 함수입니다. category 값을 기반으로 컬러를 결정합니다.
+
+    Parameters:
+    category (str) : 과목이 전선, 전기, 교필 중에 무엇인지 알려주는 문자열입니다.
+
+    return:
+    colors 에서 알맞은 값을 찾아 해당 컬러의 문자열을 반환해줍니다. 아무것도 해당하지 않는다면 black을 반환합니다.
+    """
+
     colors = {
         '전기': 'red',
         '전선': 'blue',
@@ -73,6 +115,20 @@ def get_edge_color(category: str) -> str:
 
 
 def draw_course_structure(subjects: Optional[strictyaml.YAML], output_file: str, width: int, height: int):
+    
+    """
+    파싱된 데이터를 기반으로, 과목의 위치를 조정하고, matplotlib로 데이터를 그린 후 output_file 경로로 파일을 저장하는 함수입니다.
+
+    Parameters:
+    subjects (Optional[strictyaml.YAML]) : 파싱된 과목 부분의 strictyaml.YAML 유형 데이터를 받습니다.
+    output_file (str) : 출력 데이터를 저장할 경로와 이름입니다.
+    width (int) : 사용자 임의 너비값입니다.
+    height (int) : 사용자 임의 높이값입니다.
+
+    return:
+    키값쌍이 과목, 학기 로 구성되어 있는 dict 자료형을 반환합니다.
+    """
+
     font_name = get_system_font()[0]['name']
 
     rc('font', family=font_name)
@@ -86,7 +142,7 @@ def draw_course_structure(subjects: Optional[strictyaml.YAML], output_file: str,
         grade = int(subject['학년'])
         semester = int(subject['학기'])
         x = grade
-        y = adjusted_pos[grade].pop(0)
+        y = adjusted_pos[grade].pop(0) # + semester : 학기간 간격을 주고싶다면 주석을 풀것.
         ref[ind] = [(x, y)]
         ind += 1
 
@@ -145,16 +201,17 @@ def draw_course_structure(subjects: Optional[strictyaml.YAML], output_file: str,
     plt.xlabel('학년')
     plt.ylabel('학기')
     plt.subplots_adjust(left=0.07, bottom=0.07, right=0.98, top=0.9)
-    plt.xticks(range(1, 5))
-    plt.yticks(range(1, 3))
+    plt.xticks(range(1, 5)) # 학년
+    plt.yticks(range(1, 3)) # 학기
     plt.gca().invert_yaxis()
-    plt.grid(True)
+    plt.grid(True) # 그리드 표시
 
     min_y = min(y for _, y in pos.values())
     max_y = max(y for _, y in pos.values())
     center_y = (min_y + max_y) / 1.75
     plt.axhline(center_y, color='black', linestyle='-', linewidth=2)
 
+    # 학년별로 배경색 설정
     for grade in range(1, 5):
         if grade % 2 == 0:
             plt.axvspan(grade - 0.5, grade + 0.5, color='lightgray', alpha=0.5)
@@ -164,7 +221,9 @@ def draw_course_structure(subjects: Optional[strictyaml.YAML], output_file: str,
     categories = ['전기', '전선', '교필']
     colors = ['red', 'blue', 'green']
     
-    patches = []
+    patches = [] # Patch 객체들을 담을 리스트 초기화
+
+    # 각 범주와 색상 쌍을 처리하여 Patch 객체 생성
     for category, color in zip(categories, colors):
         patch = mpatches.Patch(color=color, label=category)
         patches.append(patch)
