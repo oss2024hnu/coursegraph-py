6ca7695f5b1c229ac8da599d69e2fe7295bac455
Han <lovingamtist@gmail.com>
2024-06-17 21:37:27 +0900

    pr전

--- coursegraph/show_graph.py
+++ coursegraph/show_graph.py
@@ -190,8 +190,8 @@ def draw_course_structure(subjects: Optional[strictyaml.YAML], output_file: str,
     bbox_props = dict(boxstyle=f"round,pad=0.5", ec='black', lw=2, facecolor='white')
     for x in range(1,5):
         plt.text(x,-0.18, f"{x}학년", fontsize=18, ha='center', va='center', fontweight='bold', bbox=bbox_props)
-    for y in range(1,3):
-        plt.text(0.6,(y-1)+0.5, f"{y}학기", fontsize=18, ha='center', va='center', fontweight='bold', bbox=bbox_props)
+    for y in range(0,2):
+        plt.text(0.6,y+0.5, f"{y}학기", fontsize=18, ha='center', va='center', fontweight='bold', bbox=bbox_props)
 
     nx.draw_networkx_edges(G, pos, edgelist=edge_attrs.edgelist,
                        arrowstyle=edge_attrs.arrowstyle,
